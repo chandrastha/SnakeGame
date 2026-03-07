710f42f4fcf88f0a06b16cf29807afe1a0b66b5d
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2026-03-06 21:57:07 -0500

    Reduce offline frame drops with bot render LOD and timing fix

--- SnakeGame/GameScene.swift
+++ SnakeGame/GameScene.swift
@@ -324,6 +324,9 @@ class GameScene: SKScene {
     let botBoostCooldownRange: ClosedRange<CGFloat> = 1.10...2.40
     let botDetailedAIRadius: CGFloat = 1200.0
     let botCollisionBroadPhaseRadius: CGFloat = 260.0
+    let botActivationDistance: CGFloat = 920.0
+    let botDeactivationDistance: CGFloat = 1120.0
+    var botVisibilityUpdateTimer: CGFloat = 0
     var frameCounter = 0
     var botBodyUpdateFrame: Int = 0
 
@@ -464,6 +467,7 @@ class GameScene: SKScene {
         botUpdateAccumulator = 0
         botCollisionAccumulator = 0
         botHeadCheckAccumulator = 0
+        botVisibilityUpdateTimer = 0
         leaderboardUpdateTimer = 0
         minimapUpdateTimer     = 0
         leaderArrowUpdateTimer = 0
@@ -2689,6 +2693,27 @@ class GameScene: SKScene {
         }
     }
 
+    private func updateBotVisibilityLOD() {
+        guard gameMode != .online, !isSpecialOfflineMode else { return }
+
+        let playerPos = snakeHead.position
+        let activateSq = botActivationDistance * botActivationDistance
+        let deactivateSq = botDeactivationDistance * botDeactivationDistance
+
+        for i in bots.indices {
+            guard !bots[i].isDead else { continue }
+            let dx = bots[i].position.x - playerPos.x
+            let dy = bots[i].position.y - playerPos.y
+            let distSq = dx * dx + dy * dy
+
+            if bots[i].isActive {
+                if distSq > deactivateSq { deactivateBot(i) }
+            } else {
+                if distSq < activateSq { activateBot(i) }
+            }
+        }
+    }
+
     private func botPersonalities(for tier: BotTier) -> [BotPersonalityKind] {
         switch tier {
         case .easy:
@@ -4138,9 +4163,10 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         guard !isGameOver, gameSetupComplete, gameStarted, !isPausedGame else { return }
 
-        let dt: CGFloat = lastUpdateTime == 0
+        let frameDelta: CGFloat = lastUpdateTime == 0
             ? CGFloat(1.0 / 60.0)
-            : CGFloat(min(currentTime - lastUpdateTime, maxDeltaTime))
+            : CGFloat(max(0, currentTime - lastUpdateTime))
+        let dt: CGFloat = min(frameDelta, CGFloat(maxDeltaTime))
         lastUpdateTime = currentTime
 
         updatePowerUps(dt: dt)
@@ -4278,6 +4304,14 @@ class GameScene: SKScene {
         updateCamera()
         updateHUDPositions()
 
+        if gameMode != .online && !isSpecialOfflineMode {
+            botVisibilityUpdateTimer += dt
+            if botVisibilityUpdateTimer >= 0.20 {
+                botVisibilityUpdateTimer = 0
+                updateBotVisibilityLOD()
+            }
+        }
+
         // --- Magnet power-up: pull food every other frame ---
         if magnetActive && frameCounter % 2 == 0 { applyMagnetEffect() }
 
@@ -4288,9 +4322,10 @@ class GameScene: SKScene {
             updateSnakeRaceMode(dt: dt)
         } else if gameMode != .online {
             let simulationStep = CGFloat(1.0 / minimumGameplayFPS)
-            botUpdateAccumulator += dt
-            botCollisionAccumulator += dt
-            botHeadCheckAccumulator += dt
+            let botDelta = min(frameDelta, 0.2)
+            botUpdateAccumulator += botDelta
+            botCollisionAccumulator += botDelta
+            botHeadCheckAccumulator += botDelta
 
             while botUpdateAccumulator >= simulationStep {
                 updateBots(dt: simulationStep, updateAI: true)
